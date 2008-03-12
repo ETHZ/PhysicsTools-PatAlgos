45e8aa6a397cccd3b6c0c3f23d71989bac4acb9c
cmsbuild <>
2008-03-12 22:08:53 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-V00-00-05'.
    Sprout from master 2008-03-12 17:14:06 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> '*** empty log message ***'
    Cherrypick from master 2008-01-23 18:21:27 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Tools for back-references and association skimmers,':
        test/RefTestAnalyzer.cc
    Cherrypick from master 2008-03-07 12:47:26 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Initial commit of PAT L1 matching for layer0 including *.cfi files for em objects (photons, electron), muons and MET).':
        plugins/PATL1Producer.cc
    Cherrypick from PhysicsTools-PatAlgos-B1_6_X 2008-03-12 22:08:49 UTC Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch> 'Added the missing plugin definition.':
        plugins/JetTagsToValueMap.cc
        plugins/PATHLTMatcher.cc
        plugins/PATHLTProducer.cc
        plugins/PATJetProducer.cc
        plugins/PATJetProducer.h
        plugins/PATMCMatcher.cc
        plugins/PATMuonProducer.cc
        plugins/PATPhotonCleaner.cc
        plugins/PATPhotonCleaner.h
        plugins/PATPhotonCleaner.icc
        plugins/PATTauProducer.cc
        plugins/ValueMapSkimmers.cc
        src/PATPrimaryVertexSelector.cc

--- plugins/PATPhotonCleaner.cc
+++ plugins/PATPhotonCleaner.cc
@@ -1,6 +1,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 #include "PhysicsTools/PatAlgos/plugins/PATPhotonCleaner.h"
+#include "PhysicsTools/PatAlgos/plugins/PATPhotonCleaner.icc"
 
 using namespace pat;
 DEFINE_FWK_MODULE(PATBasePhotonCleaner);
-//DEFINE_FWK_MODULE(PATConvertedPhotonCleaner);
+DEFINE_FWK_MODULE(PATConvertedPhotonCleaner);
