85cbd7cea6d518fc94ae765b85a8b4cfb30e1385
cmsbuild <>
2008-06-03 22:24:29 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-V02-06-00'.
    Sprout from PhysicsTools-PatAlgos-B2_0_X 2008-06-03 22:24:28 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Adding support for generic candidates from tracks'
    Delete:
        python/cleaningLayer0/caloJetCleaner_cfi.py
        python/cleaningLayer0/caloMetCleaner_cfi.py
        python/cleaningLayer0/electronCleaner_cfi.py
        python/cleaningLayer0/muonCleaner_cfi.py
        python/cleaningLayer0/pfTauCleaner_cfi.py
        python/cleaningLayer0/photonCleaner_cfi.py
        python/mcMatchLayer0/electronMatch_cfi.py
        python/mcMatchLayer0/jetMatch_cfi.py
        python/mcMatchLayer0/mcMatchSequences_cff.py
        python/mcMatchLayer0/muonMatch_cfi.py
        python/mcMatchLayer0/photonMatch_cfi.py
        python/mcMatchLayer0/tauMatch_cfi.py
        python/producersLayer1/electronProducer_cff.py
        python/producersLayer1/electronProducer_cfi.py
        python/producersLayer1/hemisphereProducer_cff.py
        python/producersLayer1/hemisphereProducer_cfi.py
        python/producersLayer1/jetProducer_cff.py
        python/producersLayer1/jetProducer_cfi.py
        python/producersLayer1/metProducer_cff.py
        python/producersLayer1/metProducer_cfi.py
        python/producersLayer1/muonProducer_cff.py
        python/producersLayer1/muonProducer_cfi.py
        python/producersLayer1/photonProducer_cff.py
        python/producersLayer1/photonProducer_cfi.py
        python/producersLayer1/tauProducer_cff.py
        python/producersLayer1/tauProducer_cfi.py
        python/recoLayer0/bTagging_cff.py
        python/recoLayer0/beforeLevel0Reco_cff.py
        python/recoLayer0/electronIsolation_cff.py
        python/recoLayer0/highLevelReco_cff.py
        python/recoLayer0/jetCorrFactors_cfi.py
        python/recoLayer0/jetFlavourId_cff.py
        python/recoLayer0/jetMETCorrections_cff.py
        python/recoLayer0/muonIsolation_cff.py
        python/recoLayer0/photonIsolation_cff.py
        python/selectionLayer1/electronCountFilter_cff.py
        python/selectionLayer1/electronMaxFilter_cfi.py
        python/selectionLayer1/electronMinFilter_cfi.py
        python/selectionLayer1/electronSelector_cfi.py
        python/selectionLayer1/jetCountFilter_cff.py
        python/selectionLayer1/jetMaxFilter_cfi.py
        python/selectionLayer1/jetMinFilter_cfi.py
        python/selectionLayer1/jetSelector_cfi.py
        python/selectionLayer1/leptonCountFilter_cfi.py
        python/selectionLayer1/metSelector_cfi.py
        python/selectionLayer1/muonCountFilter_cff.py
        python/selectionLayer1/muonMaxFilter_cfi.py
        python/selectionLayer1/muonMinFilter_cfi.py
        python/selectionLayer1/muonSelector_cfi.py
        python/selectionLayer1/photonCountFilter_cff.py
        python/selectionLayer1/photonMaxFilter_cfi.py
        python/selectionLayer1/photonMinFilter_cfi.py
        python/selectionLayer1/photonSelector_cfi.py
        python/selectionLayer1/tauCountFilter_cff.py
        python/selectionLayer1/tauMaxFilter_cfi.py
        python/selectionLayer1/tauMinFilter_cfi.py
        python/selectionLayer1/tauSelector_cfi.py

--- python/selectionLayer1/jetSelector_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-selectedLayer1Jets = cms.EDFilter("PATJetSelector",
-    src = cms.InputTag("allLayer1Jets"),
-    cut = cms.string('et > 15. & abs(eta) < 2.4 & nConstituents > 0')
-)
-
-
