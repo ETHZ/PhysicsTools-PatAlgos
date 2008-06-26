784584fbcefcba11f18da2ffd48a3c4cf9ebdcda
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-26 21:04:15 +0000

    translate from the cff original

--- python/recoLayer0/jetMETCorrections_cff.py
+++ python/recoLayer0/jetMETCorrections_cff.py
@@ -1,7 +1,36 @@
 import FWCore.ParameterSet.Config as cms
 
+# define jet flavour correction services
 from JetMETCorrections.Configuration.MCJetCorrections152_cff import *
 from JetMETCorrections.Configuration.L5FlavorCorrections_cff import *
+# produce associated jet correction factors in a valuemap
+from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
+# default JetMET calibration on IC, KT and MC Jets
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
-patJetMETCorrections = cms.Sequence(corMetType1Icone5)
+import JetMETCorrections.Type1MET.corMetMuons_cfi
+# JetMET corrections for muons: input jet-corrected MET
+# It would be better to get this config to JetMETCorrections/Type1MET/data/ at some point
+corMetType1Icone5Muons = JetMETCorrections.Type1MET.corMetMuons_cfi.corMetMuons.clone()
+layer0JetCorrFactors = cms.EDFilter("JetCorrFactorsValueMapSkimmer",
+    association = cms.InputTag("jetCorrFactors"),
+    collection = cms.InputTag("allLayer0Jets"),
+    backrefs = cms.InputTag("allLayer0Jets")
+)
+
+# Jet corrections are now done on the fly in the JetProducer
+# default PAT sequence for JetMET corrections
+patAODJetMETCorrections = cms.Sequence(jetCorrFactors*corMetType1Icone5*corMetType1Icone5Muons)
+patLayer0JetMETCorrections = cms.Sequence(layer0JetCorrFactors)
+corMetType1Icone5Muons.inputUncorMetLabel = 'corMetType1Icone5'
+corMetType1Icone5Muons.TrackAssociatorParameters.useEcal = False ## RecoHits
+
+corMetType1Icone5Muons.TrackAssociatorParameters.useHcal = False ## RecoHits
+
+corMetType1Icone5Muons.TrackAssociatorParameters.useHO = False ## RecoHits
+
+corMetType1Icone5Muons.TrackAssociatorParameters.useCalo = True ## CaloTowers
+
+corMetType1Icone5Muons.TrackAssociatorParameters.useMuon = False ## RecoHits
+
+corMetType1Icone5Muons.TrackAssociatorParameters.truthMatch = False
 
