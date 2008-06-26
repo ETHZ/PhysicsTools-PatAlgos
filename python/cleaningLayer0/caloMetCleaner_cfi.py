bcf2bc20d446c3d5b16fe1231937a33a53b40054
Slava Krutelyov <sha1-c476adc99c5854f1e14f4c9fad7f2e69a31659a2@cern.ch>
2008-06-26 21:05:22 +0000

    input jes+muon corrected met; now the comments are translated too

--- python/cleaningLayer0/caloMetCleaner_cfi.py
+++ python/cleaningLayer0/caloMetCleaner_cfi.py
@@ -1,12 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
 allLayer0METs = cms.EDFilter("PATCaloMETCleaner",
-    metSource = cms.InputTag("corMetType1Icone5"),
-    saveAll = cms.string(''),
-    bitsToIgnore = cms.vstring(),
-    wantSummary = cms.untracked.bool(True),
-    markItems = cms.bool(True),
-    saveRejected = cms.string('')
+    metSource = cms.InputTag("corMetType1Icone5Muons"),
+    saveAll = cms.string(''), ## set this to a non empty label to save a list of all items both passing and failing
+
+    bitsToIgnore = cms.vstring(), ## You can specify some bit names, e.g. "Overflow/User1", "Core/Duplicate", "Isolation/All".
+
+    wantSummary = cms.untracked.bool(True), ## print summary information for each status flag
+
+    markItems = cms.bool(True), ## write the status flags in the output items
+
+    saveRejected = cms.string('') ## set this to a non empty label to save the list of items which fail
+
 )
 
 
