f8569ba791b135084e21a78b61a058d498ada487
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2008-10-17 22:37:35 +0000

    Fixing also CaloTau example

--- test/patLayer1_fromAOD_CaloTau_full.cfg.py
+++ test/patLayer1_fromAOD_CaloTau_full.cfg.py
@@ -31,27 +31,30 @@ process.load("PhysicsTools.PatAlgos.patLayer1_cff")
 #process.content = cms.EDAnalyzer("EventContentAnalyzer")
 process.load("PhysicsTools.PatAlgos.cleaningLayer0.caloTauCleaner_cfi")
 
+process.patLayer0.replace(process.allLayer0Taus, process.allLayer0CaloTaus)
+process.patLayer0.replace(process.patPFTauDiscrimination, process.patCaloTauDiscrimination)
 # replace allLayer0Taus with allLayer0CaloTaus by hand unpacking the patLayer0 sequence
 process.p = cms.Path(
         #process.content +             # uncomment to get a dump of the input to PAT
-        process.patBeforeLevel0Reco *
-        process.allLayer0Muons *
-        process.allLayer0Electrons *
-        process.allLayer0Photons *
-        process.allLayer0CaloTaus *
-        process.allLayer0Jets *
-        process.allLayer0METs *
-        process.patHighLevelReco *
-        process.patMCTruth *
-        process.patTrigMatch +
+        process.patLayer0 +
         process.patLayer1
 )
+
+
 # reconfigure MC, Trigger match and Layer 1 to use CaloTaus
 process.tauMatch.src                 = cms.InputTag('allLayer0CaloTaus')
 process.tauGenJetMatch.src           = cms.InputTag('allLayer0CaloTaus')
 process.tauTrigMatchHLT1Tau.src      = cms.InputTag('allLayer0CaloTaus')
 process.tauTrigMatchHLT2TauPixel.src = cms.InputTag('allLayer0CaloTaus')
 process.allLayer1Taus.tauSource      = cms.InputTag('allLayer0CaloTaus')
+process.allLayer1Taus.tauIDSources = cms.PSet(
+        # configure many IDs as InputTag <someName> = <someTag>
+        # you can comment out those you don't want to save some disk space
+        leadingTrackFinding = cms.InputTag("patCaloRecoTauDiscriminationByLeadingTrackFinding"),
+        leadingTrackPtCut = cms.InputTag("patCaloRecoTauDiscriminationByLeadingTrackPtCut"),
+        byIsolation = cms.InputTag("patCaloRecoTauDiscriminationByIsolation"),
+        #againstElectron = cms.InputTag("patCaloRecoTauDiscriminationAgainstElectron"),  # Not on AOD
+)
 
 # Output module configuration
 process.out = cms.OutputModule("PoolOutputModule",
