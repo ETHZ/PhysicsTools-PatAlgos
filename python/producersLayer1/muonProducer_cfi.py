4457789457a8041d7bdf24b8029fd20726c957f5
Letizia Lusito <sha1-92dd2d99f673ea59ee4ed3d1c5ddb31e25b69603@cern.ch>
2009-03-19 17:18:59 +0000

    PFlow muons can be transformed into PatElectrons Muon modified in branch 2_2_X

--- python/producersLayer1/muonProducer_cfi.py
+++ python/producersLayer1/muonProducer_cfi.py
@@ -4,8 +4,8 @@ allLayer1Muons = cms.EDProducer("PATMuonProducer",
 
     # General configurables
     muonSource = cms.InputTag("muons"),
-    pfMuonSource = cms.InputTag("pfMuons"),
-    useParticleFlow =  cms.bool( False ),
+    
+ 
 
     # user data to add
     userData = cms.PSet(
@@ -33,38 +33,36 @@ allLayer1Muons = cms.EDProducer("PATMuonProducer",
     embedTpfmsMuon      = cms.bool(True), ## whether to embed in AOD externally stored TeV-refit TPFMS muon track
     embedPFCandidate = cms.bool(False),
 
+# resolution configurables
+    addResolutions   = cms.bool(False),
+# pflow specific
+    pfMuonSource = cms.InputTag("pfMuons"),
+    useParticleFlow =  cms.bool( False ),
+    embedPFCandidate = cms.bool(False),
+
+
     # isolation configurables
     isolation = cms.PSet(
         hcal = cms.PSet(
-            src = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
-            deltaR = cms.double(0.3)
+            src = cms.InputTag("muIsoFromDepsHcalFromTowers"),
+            #deltaR = cms.double(0.3)
         ),
         tracker = cms.PSet(
-            src = cms.InputTag("muIsoDepositTk"),
-            deltaR = cms.double(0.3)
+            src = cms.InputTag("muIsoFromDepsTk"),
+            #deltaR = cms.double(0.3)
         ),
-        user = cms.VPSet(cms.PSet(
-            src = cms.InputTag("muIsoDepositCalByAssociatorTowers","ho"),
-            deltaR = cms.double(0.3)
-            ), 
-            cms.PSet(
-                src = cms.InputTag("muIsoDepositJets"),
-                deltaR = cms.double(0.3)
-            )),
+        user = cms.VPSet(), 
         ecal = cms.PSet(
-            src = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
-            deltaR = cms.double(0.3)
+            src = cms.InputTag("eleIsoFromDepsEcalFromHits"),
+            #deltaR = cms.double(0.3)
         )
     ),
     # embed IsoDeposits to recompute isolation easily
     isoDeposits = cms.PSet(
         tracker = cms.InputTag("muIsoDepositTk"),
-        ecal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
-        hcal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
-        user    = cms.VInputTag(
-                     cms.InputTag("muIsoDepositCalByAssociatorTowers","ho"), 
-                     cms.InputTag("muIsoDepositJets")
-                  ),
+        ecal    = cms.InputTag("muIsoDepositEcalFromHits"),
+        hcal    = cms.InputTag("muIsoDepositHcalFromTowers"),
+
     ),
 
     # Resolution configurables
