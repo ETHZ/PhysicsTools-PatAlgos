6e0934e386c1b0343ee0b59b76e817dadba294d7
cmsbuild <>
2009-04-14 10:10:06 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-V05-03-09'.
    Sprout from PhysicsTools-PatAlgos-B2_2_X 2009-04-14 10:10:05 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'If default JEC is Summer08Redigi, it must be changed here (when switching'
    Cherrypick from PhysicsTools-PatAlgos-B2_2_X 2009-03-31 13:45:49 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Should remove the duplicate photons cfi file even from here':
        python/recoLayer0/aodReco_cff.py

--- python/recoLayer0/aodReco_cff.py
+++ python/recoLayer0/aodReco_cff.py
@@ -9,8 +9,6 @@ from PhysicsTools.PatAlgos.recoLayer0.muonIsolation_cff import *
 from PhysicsTools.PatAlgos.recoLayer0.photonId_cff import *
 from PhysicsTools.PatAlgos.recoLayer0.photonIsolation_cff import *
 from PhysicsTools.PatAlgos.recoLayer0.tauDiscriminators_cff import *
-from PhysicsTools.PatAlgos.recoLayer0.pfCandidateIsoDepositSelection_cff import *
-from PhysicsTools.PatAlgos.recoLayer0.tauIsolation_cff import *
 
 # These two duplicate removals are here because they're AOD bugfixes
 from PhysicsTools.PatAlgos.recoLayer0.duplicatedElectrons_cfi import *
@@ -33,10 +31,8 @@ patAODExtraReco = cms.Sequence(
     patJetTracksCharge +
     #patMuonIsolation +   # Empty sequences not supported yet
     #patPhotonID +        # Empty sequences not supported yet
-    patPhotonIsolation +
+    patPhotonIsolation 
     #patTauDiscrimination # Empty sequences not supported yet
-    patPFCandidateIsoDepositSelection +
-    patPFTauIsolation
 )
 
 # One module to count some AOD Objects that are usually input to PAT
