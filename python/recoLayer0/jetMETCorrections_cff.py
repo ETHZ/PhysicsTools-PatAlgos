723609075b077780dd7104d4a45e796f06ce9fcf
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-05-15 16:03:19 +0000

    adaptations to new MET Muon corrections

--- python/recoLayer0/jetMETCorrections_cff.py
+++ python/recoLayer0/jetMETCorrections_cff.py
@@ -11,20 +11,12 @@ from JetMETCorrections.Configuration.L2L3Corrections_Summer08_cff import *
 corMetType1Icone5.corrector = cms.string('L2L3JetCorrectorIC5Calo')
 
 ## MET corrections for muons:
-from Geometry.CommonDetUnit.globalTrackingGeometry_cfi import *
-from TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff import *
-from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi import *
-from JetMETCorrections.Type1MET.MetMuonCorrections_cff import corMetGlobalMuons, goodMuonsforMETCorrection
-corMetType1Icone5Muons = corMetGlobalMuons.clone(uncorMETInputTag = cms.InputTag('corMetType1Icone5'),
-                                                 muonsInputTag    = cms.InputTag('goodMuonsforMETCorrection'))
-corMetType1Icone5Muons.TrackAssociatorParameters.useEcal    = False ## RecoHits
-corMetType1Icone5Muons.TrackAssociatorParameters.useHcal    = False ## RecoHits
-corMetType1Icone5Muons.TrackAssociatorParameters.useHO      = False ## RecoHits
-corMetType1Icone5Muons.TrackAssociatorParameters.useCalo    = True  ## CaloTowers
-corMetType1Icone5Muons.TrackAssociatorParameters.useMuon    = False ## RecoHits
-corMetType1Icone5Muons.TrackAssociatorParameters.truthMatch = False
+from JetMETCorrections.Type1MET.MuonMETValueMapProducer_cff import *
+from JetMETCorrections.Type1MET.MetMuonCorrections_cff import corMetGlobalMuons
+corMetType1Icone5Muons = corMetGlobalMuons.clone(uncorMETInputTag = cms.InputTag('corMetType1Icone5'))
+
 ## MET sequence
-patMETCorrections = cms.Sequence(goodMuonsforMETCorrection *
+patMETCorrections = cms.Sequence(muonMETValueMapProducer *
                                  corMetType1Icone5 *
                                  corMetType1Icone5Muons
                                  )
@@ -33,4 +25,3 @@ patMETCorrections = cms.Sequence(goodMuonsforMETCorrection *
 patJetMETCorrections = cms.Sequence(patJetCorrections +
                                     patMETCorrections
                                     )
-
