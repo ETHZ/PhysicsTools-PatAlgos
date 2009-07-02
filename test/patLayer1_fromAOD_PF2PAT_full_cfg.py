b8ddd50c6a5453bb7c90f0218450afaba0bcb8f7
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-07-02 12:31:59 +0000

    - adapted to changes in the PF2PAT, on the isolation side. - muon isolation with isodeposits re-implemented in PF2PAT, and working. detailed studies on particle based isolation are needed. - modifications in the PATMuonProducer: * embedding of particle based IsoDeposits * embedding of pre-computed isolation values * in case PF2PAT is used, the momentum of the pat::Muon is taken from the PFCandidate, not from the corresponding reco::Muon. - Redefined the enum in Isolation.h for a more accurate meaning.

--- test/patLayer1_fromAOD_PF2PAT_full_cfg.py
+++ test/patLayer1_fromAOD_PF2PAT_full_cfg.py
@@ -2,33 +2,32 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("PAT")
 
-process.source = cms.Source("PoolSource", 
-     fileNames = cms.untracked.vstring('file:aod.root')
-)
-#process.load("PhysicsTools.PFCandProducer.Sources/source_ZtoMus_DBS_cfi")
 
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+# control flags
+localSource = False
+addPF2PATOutput = False
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V1::All')
-process.load("Configuration.StandardSequences.MagneticField_cff")
+# source 
+process.load("PhysicsTools.PFCandProducer.Sources/source_ZtoMus_DBS_cfi")
+if localSource:
+    process.source = cms.Source("PoolSource", 
+                                fileNames = cms.untracked.vstring('file:aod.root')
+                                )
+
 
 # PF2PAT
 process.load("PhysicsTools.PFCandProducer.PF2PAT_cff")
 # PAT Layer 0+1
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
-#process.content = cms.EDAnalyzer("EventContentAnalyzer")
 
 # Configure PAT to use PF2PAT instead of AOD sources
 from PhysicsTools.PatAlgos.tools.pfTools import *
-print "coucou"
+
 usePF2PAT(process,runPF2PAT=True)  # or you can leave this to the default, False, and run PF2PAT before patDefaultSequence
 
-#process.allLayer1Taus.addTauID = False
+
 
 
 process.p = cms.Path(
@@ -36,6 +35,7 @@ process.p = cms.Path(
 )
 
 # Output module configuration
+
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoLayer1Cleaning
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('patLayer1_fromAOD_PF2PAT_full.root'),
@@ -44,6 +44,20 @@ process.out = cms.OutputModule("PoolOutputModule",
     # save PAT Layer 1 output
     outputCommands = cms.untracked.vstring('drop *', *patEventContentNoLayer1Cleaning ) # you need a '*' to unpack the list of commands 'patEventContentNoLayer1Cleaning'
 )
+
+if addPF2PATOutput:
+    process.load("PhysicsTools.PFCandProducer.PF2PAT_EventContent_cff")
+    process.out.outputCommands.extend( process.PF2PATEventContent.outputCommands)
+    process.out.outputCommands.extend( process.PF2PATStudiesEventContent.outputCommands)
+    
 process.outpath = cms.EndPath(process.out)
 
-#print process.dumpPython()
+# various necessary stuff
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('MC_31X_V1::All')
+process.load("Configuration.StandardSequences.MagneticField_cff")
--- test/patLayer1_fromPF2PAT_cfg.py
+++ test/patLayer1_fromPF2PAT_cfg.py
@@ -2,9 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("PAT")
 
-# initialize MessageLogger and output report
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
 
 # source
 process.source = cms.Source("PoolSource", 
@@ -12,18 +9,15 @@ process.source = cms.Source("PoolSource",
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('IDEAL_31X::All')
-process.load("Configuration.StandardSequences.MagneticField_cff")
 
 # PAT Layer 0+1
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
-#process.content = cms.EDAnalyzer("EventContentAnalyzer")
 
 # Configure PAT to use PF2PAT instead of AOD sources
 from PhysicsTools.PatAlgos.tools.pfTools import *
-usePF2PAT(process)
+
+usePF2PAT(process, runPF2PAT=False)
+
 
 process.p = cms.Path(
     process.patDefaultSequence  
@@ -43,3 +37,13 @@ process.out = cms.OutputModule("PoolOutputModule",
 )
 process.outpath = cms.EndPath(process.out)
 
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('MC_31X_V1::All')
+process.load("Configuration.StandardSequences.MagneticField_cff")
