52fe3bd1b2bb0fe5923d8b933c50137616d4f7da
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-07-17 16:10:02 +0000

    now also the switchJetCollection function is in place

--- test/patLayer1_fromAOD_sisCone_full_cfg.py
+++ test/patLayer1_fromAOD_sisCone_full_cfg.py
@@ -1,44 +1,31 @@
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
-# PAT Layer 0+1
+# load the standard PAT config 
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 from PhysicsTools.PatAlgos.tools.jetTools import *
 
-## ==== Example with CaloJets
+# uncomment the following lines to switch the standard jet collection
+# in PAT (iterativeCone5) to sisCone5
 switchJetCollection(process, 
-        cms.InputTag('sisCone5CaloJets'),  # Jet collection; must be already in the event when patDefaultSequence is executed
-        doJTA=True,            # Run Jet-Track association & JetCharge
-        doBTagging=True,       # Run b-tagging
-        jetCorrLabel=('SC5','Calo'), # example jet correction name; set to None for no JEC
-        doType1MET=True,       # recompute Type1 MET using these jets
-        genJetCollection=cms.InputTag("sisCone5GenJets")) 
+                    cms.InputTag('sisCone5CaloJets'),   
+                    doJTA            = True,            
+                    doBTagging       = True,            
+                    jetCorrLabel     = ('SC5','Calo'),  
+                    doType1MET       = True,            
+                    genJetCollection = cms.InputTag("sisCone5GenJets")
+                    ) 
 
-## ==== FOR BASIC JETS
-### make some basic jets for testing
-# from RecoJets.JetProducers.BasicJetIcone5_cfi import iterativeCone5BasicJets
-# process.iterativeCone5BasicJets = iterativeCone5BasicJets.clone(src = cms.InputTag("towerMaker"))
-#
-### configure PAT
-# switchJetCollection(process, 
-#        cms.InputTag('iterativeCone5BasicJets'), # Jet collection; must be already in the event patLayer0 sequence is executed
-#        doJTA=True,
-#        doBTagging=True,
-#        jetCorrLabel=None,#=('S5','Calo'), # If you have JES corrections, you can apply them even to BasicJets
-#        doType1MET=False)                  # Type1MET dows not work on BasicJets :-(
-
-
-# let it run 
 process.p = cms.Path(
                 process.patDefaultSequence  
             )
 
 # In addition you usually want to change the following parameters:
 #
-#   process.GlobalTag.globaltag =  ...      (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
-#   process.source.fileNames = [ ... ]      (e.g. 'file:AOD.root')
-#   process.maxEvents.input = ...           (e.g. -1 to run on all events)
-#   process.out.outputCommands = [ ... ]    (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
-#   process.out.fileName = ...              (e.g. 'myTuple.root')
-#   process.options.wantSummary = False     (to suppress the long output at the end of the job)
+#   process.GlobalTag.globaltag =  ...     ## (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+#   process.source.fileNames = [ ... ]     ## (e.g. 'file:AOD.root')
+process.maxEvents.input = 10               ## (e.g. -1 to run on all events)
+#   process.out.outputCommands = [ ... ]   ## (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#   process.out.fileName = ...             ## (e.g. 'myTuple.root')
+process.options.wantSummary = False        ## (to suppress the long output at the end of the job)
 
