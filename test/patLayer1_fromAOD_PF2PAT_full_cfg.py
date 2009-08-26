9031a74965892c60dbff094f2f3fa6fb691ec915
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2009-08-26 08:34:06 +0000

    The PATElectronProducer is now working in the PF2PAT case as well: - IsoDeposits and IsolationValues can be embedded in the pat::Electron - the PF information can now be accessed from the pat::Electron
    Names for particle based IsoDeposits normalized and shortened.
    
    Todo:
    - study and tune the particle-based isolation for electrons and muons.

--- test/patLayer1_fromAOD_PF2PAT_full_cfg.py
+++ test/patLayer1_fromAOD_PF2PAT_full_cfg.py
@@ -1,13 +1,17 @@
+
+
+
 # This is an example PAT configuration showing the usage of PF2PAT
 
 # Starting with a skeleton process which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 # overriding source and various other things
+#process.load("PhysicsTools.PFCandProducer.Sources.source_ZtoEles_DBS_312_cfi")
 process.load("PhysicsTools.PFCandProducer.Sources.source_ZtoMus_DBS_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.out.fileName = cms.untracked.string('patLayer1_fromAOD_PF2PAT_full.root')
 
 # load the standard PAT config
@@ -18,18 +22,21 @@ process.load("PhysicsTools.PFCandProducer.PF2PAT_cff")
 
 # Configure PAT to use PF2PAT instead of AOD sources
 from PhysicsTools.PatAlgos.tools.pfTools import *
-usePF2PAT(process,runPF2PAT=True)  # or you can leave this to the default, False, and run PF2PAT before patDefaultSequence
+usePF2PAT(process,runPF2PAT=True, addElectrons=True)  # or you can leave this to the default, False, and run PF2PAT before patDefaultSequence
 
 # Let it run
 process.p = cms.Path(
     process.patDefaultSequence  
 )
 
+process.allLayer1Electrons.embedGenMatch = True
+process.allLayer1Muons.embedGenMatch = True
+
 # Add PF2PAT output to the created file
 from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoLayer1Cleaning
 process.load("PhysicsTools.PFCandProducer.PF2PAT_EventContent_cff")
-process.out.outputCommands =  cms.untracked.vstring('drop *', *patEventContentNoLayer1Cleaning )
-process.out.outputCommands.extend( process.PF2PATEventContent.outputCommands)
+process.out.outputCommands =  cms.untracked.vstring('drop *')
+process.out.outputCommands.extend( process.PATEventContent.outputCommands)
 
 
 # In addition you usually want to change the following parameters:
@@ -41,4 +48,4 @@ process.out.outputCommands.extend( process.PF2PATEventContent.outputCommands)
 #   process.out.fileName = ...              (e.g. 'myTuple.root')
 #   process.options.wantSummary = False     (to suppress the long output at the end of the job)
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
