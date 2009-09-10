bc090298b2b8184de0ef2e859d8e436fbba10d21
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-09-10 08:26:31 +0000

    Adding anti-kt example. Since this is the default now recommended by the jetmet group, and the facts that most users will not currently use L7 corrections, and that the L7 correction is not available for anti-kT, the L7 corrections are now switched off by default until they are available for anti-kT.
    This is very temporary until the relevant study can be done.

---
+++ test/patLayer1_fromAOD_antikT_full_cfg.py
@@ -0,0 +1,39 @@
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+# load the standard PAT config 
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+# load the anti-kt algorithm
+process.load("RecoJets.Configuration.RecoJets_cff")
+process.load("RecoJets.Configuration.RecoGenJets_cff")
+process.load("RecoJets.Configuration.GenJetParticles_cff")
+
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+# uncomment the following lines to switch the standard jet collection
+# in PAT (iterativeCone5) to ak5 (anti-kt cone = 0.5)
+switchJetCollection(process, 
+                    cms.InputTag('ak5CaloJets'),   
+                    doJTA            = True,            
+                    doBTagging       = True,            
+                    jetCorrLabel     = ('AK5','Calo'),  
+                    doType1MET       = True,            
+                    genJetCollection = cms.InputTag("ak5GenJets")
+                    ) 
+
+process.p = cms.Path(
+                process.genParticlesForJets *
+                process.ak5GenJets *
+                process.ak5CaloJets *
+                process.patDefaultSequence  
+            )
+
+# In addition you usually want to change the following parameters:
+#
+#   process.GlobalTag.globaltag =  ...     ## (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+#   process.source.fileNames = [ ... ]     ## (e.g. 'file:AOD.root')
+process.maxEvents.input = 10               ## (e.g. -1 to run on all events)
+#   process.out.outputCommands = [ ... ]   ## (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#   process.out.fileName = ...             ## (e.g. 'myTuple.root')
+process.options.wantSummary = False        ## (to suppress the long output at the end of the job)
+
