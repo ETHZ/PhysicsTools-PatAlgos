a7aef1419b0ff49774ebd107a843045548d9a39b
cmsbuild <>
2009-10-02 14:50:13 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-prunedAODForPF2PAT-31X'.
    Sprout from PhysicsTools-PatAlgos-B3_1_X 2009-09-21 12:57:18 UTC Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch> 'Backport of 33X functionality (storage of GenJet ref. instead of embedding)'
    Cherrypick from master 2009-10-02 14:50:12 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'cleaned up the PF2PAT mode in order to be able to run on the reduced AOD format.':
        python/tools/pfTools.py
        test/patLayer1_fromAOD_PF2PAT_full_cfg.py

--- test/patLayer1_fromAOD_PF2PAT_full_cfg.py
+++ test/patLayer1_fromAOD_PF2PAT_full_cfg.py
@@ -1,26 +1,28 @@
-
-
-
-# This is an example PAT configuration showing the usage of PF2PAT
+# This is an example PAT configuration showing the usage of PF2PAT+PAT
 
 # Starting with a skeleton process which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+# the source is already defined in patTemplate_cfg.
 # overriding source and various other things
-process.load("PhysicsTools.PFCandProducer.Sources.source_ZtoEles_DBS_312_cfi")
+#process.load("PhysicsTools.PFCandProducer.Sources.source_ZtoEles_DBS_312_cfi")
+#process.source = cms.Source("PoolSource", 
+#     fileNames = cms.untracked.vstring('file:aod.root')
+#)
+
+
 # process.load("PhysicsTools.PFCandProducer.Sources.source_ZtoMus_DBS_cfi")
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 process.out.fileName = cms.untracked.string('patLayer1_fromAOD_PF2PAT_full.root')
 
-# load the standard PAT config
+# load the PAT config
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-# load the PF to PAT config
-process.load("PhysicsTools.PFCandProducer.PF2PAT_cff")
-
 # Configure PAT to use PF2PAT instead of AOD sources
+# this function will modify the PAT sequences. It is currently 
+# not possible to run PF2PAT+PAT and standart PAT at the same time
 from PhysicsTools.PatAlgos.tools.pfTools import *
 usePF2PAT(process,runPF2PAT=True)  # or you can leave this to the default, False, and run PF2PAT before patDefaultSequence
 
@@ -48,4 +50,4 @@ process.out.outputCommands.extend( process.PATEventContent.outputCommands)
 #   process.out.fileName = ...              (e.g. 'myTuple.root')
 #   process.options.wantSummary = False     (to suppress the long output at the end of the job)
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
