ed3829a30958e612cd1927e8fe5695adfc866a26
cmsbuild <>
2009-11-24 16:31:36 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-CMSSW_3_3_5'.
    Sprout from master 2009-10-23 09:24:04 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'photons will not be cleaned from electrons on the request of Egamma -- nevertheless overlaps are still marked (just as an example of the potential of cleaning)'
    Cherrypick from master 2009-10-29 14:56:45 UTC Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch> 'the adapted to new structure of patDefaultSequence':
        python/tools/pfTools.py
        python/tools/tauTools.py
        test/patLayer1_fromPF2PAT_cfg.py
    Cherrypick from PhysicsTools-PatAlgos-B3_3_X 2009-11-24 16:31:35 UTC Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch> 'Add "plot all pat objects" fwlite macro':
        test/fwlite/plotAllPATObjects.C
        test/patLayer1_fromAOD_rereco09_full_cfg.py
        test/patLayer1_fromRECO_data_cfg.py
    Delete:
        test/patLayer1_fromScratch_fast_cfg.py

---
+++ test/patLayer1_fromRECO_data_cfg.py
@@ -0,0 +1,42 @@
+# This is an example PAT configuration showing the usage of PAT on minbias data
+
+# Starting with a skeleton process which gets imported with the following line
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
+
+#switch off new tau features introduced in 33X to restore 31X defaults
+# new feaures: - shrinkingConeTaus instead of fixedCone ones
+#              - TaNC discriminants attached for shrinkingConeTaus
+#              - default preselection on cleaningLayer1
+from PhysicsTools.PatAlgos.tools.tauTools import *
+switchTo31Xdefaults(process)
+
+# turn off MC matching for the process
+removeMCMatching(process, 'All')
+
+# let it run
+process.p = cms.Path(
+    process.patDefaultSequence
+    )
+
+
+# Add the files for run 122314
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring()
+
+readFiles.extend( [
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/F62B040F-6CD8-DE11-9007-001D09F24664.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/F4387297-74D8-DE11-996C-001D09F24F1F.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/EE7B1AC4-6CD8-DE11-97BB-0030487A1FEC.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/9264A28F-87D8-DE11-83A1-001D09F24763.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/7C7495C2-71D8-DE11-ACF2-001D09F248F8.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/4CAB3B6C-6BD8-DE11-845C-000423D9890C.root',
+       '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/122/314/02E8544C-70D8-DE11-85CF-001617C3B66C.root' ] );
+process.source.fileNames = readFiles
+process.source.firstRun = cms.untracked.uint32(122314)
+
+# process all the events
+process.maxEvents.input = -1
+process.options.wantSummary = True        ##  (to suppress the long output at the end of the job)    
