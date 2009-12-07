9bea7cbe690db1bf5341aa921dccd3069c20bdad
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-12-07 09:05:49 +0000

    Remove prepended directory from output location

--- test/patLayer1_fromRECO_data_123575_123596_cfg.py
+++ test/patLayer1_fromRECO_data_123575_123596_cfg.py
@@ -77,7 +77,7 @@ process.p = cms.Path(
     )
 
 # rename output file
-process.out.fileName = cms.untracked.string('/uscms_data/d2/rappocc/data/BeamCommissioning09/reco_123575_123596_pat.root')
+process.out.fileName = cms.untracked.string('reco_123575_123596_pat.root')
 
 # reduce verbosity
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
