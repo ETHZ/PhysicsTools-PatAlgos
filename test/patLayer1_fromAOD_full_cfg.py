e16e8a4303e079f7f7cd2eeea24fe329d0d656e1
Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch>
2010-03-05 00:19:19 +0000

    PFTauFixedConeHighEff removed, PFTauHPS added (redone)

--- test/patLayer1_fromAOD_full_cfg.py
+++ test/patLayer1_fromAOD_full_cfg.py
@@ -18,7 +18,7 @@ from PhysicsTools.PatAlgos.tools.coreTools import *
 #              - TaNC discriminants attached for shrinkingConeTaus
 #              - default preselection on cleaningLayer1
 from PhysicsTools.PatAlgos.tools.tauTools import *
-#switchTo31Xdefaults(process, process.patTaus)
+#switchTo31Xdefaults(process)
 
 # let it run
 process.p = cms.Path(
