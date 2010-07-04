f5573702e796f7d9666815355d932f3382e52e6d
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2010-07-04 22:24:40 +0000

    Making the production of The L1 algorithm collection optional. Due to problems with the L1 event content/setup, the PATTriggerProducer causes extreme verbosity from error messages. Due to this, the production of the L1 algorithm collection has been made optional by the new configuration parameter addL1Algos = cms.bool( False ) # optional, default: 'False' of the PATTriggerProducer. The collection is still produced, but kept empty, in order to minimize the effect on the PATTriggerEventProducer.

--- plugins/PATTriggerProducer.h
+++ plugins/PATTriggerProducer.h
@@ -7,7 +7,7 @@
 // Package:    PatAlgos
 // Class:      pat::PATTriggerProducer
 //
-// $Id: PATTriggerProducer.h,v 1.9 2010/04/20 20:03:36 vadler Exp $
+// $Id: PATTriggerProducer.h,v 1.10 2010/05/18 12:33:29 vadler Exp $
 //
 /**
   \class    pat::PATTriggerProducer PATTriggerProducer.h "PhysicsTools/PatAlgos/plugins/PATTriggerProducer.h"
@@ -16,7 +16,7 @@
    [...]
 
   \author   Volker Adler
-  \version  $Id: PATTriggerProducer.h,v 1.9 2010/04/20 20:03:36 vadler Exp $
+  \version  $Id: PATTriggerProducer.h,v 1.10 2010/05/18 12:33:29 vadler Exp $
 */
 
 
@@ -50,6 +50,7 @@ namespace pat {
       bool onlyStandAlone_; // configuration
       // L1
       L1GtUtils     l1GtUtils_;
+      bool          addL1Algos_;        // configuration (optional)
       edm::InputTag tagL1ExtraMu_;      // configuration (optional)
       edm::InputTag tagL1ExtraNoIsoEG_; // configuration (optional)
       edm::InputTag tagL1ExtraIsoEG_;   // configuration (optional)
