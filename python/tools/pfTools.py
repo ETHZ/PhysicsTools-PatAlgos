5dc68e43f46224458d2f4187d2a00ad5ea109983
cmsbuild <>
2010-09-30 11:29:41 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-Colin_384_noMoreJetPtCutInPF2PAT_Sept30'.
    Sprout from PhysicsTools-PatAlgos-B3_8_X 2010-09-08 08:12:13 UTC Michal Bluj <sha1-0b762fa2fdbd5d079389c88444d32946a6cf3409@cern.ch> 'apply correctly prefix for b-tagging (backported from HEAD)'
    Cherrypick from master 2010-09-30 11:29:40 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'removed allPfJets from the PF2PAT sequence, and updated the pfTools accordingly.':
        python/tools/pfTools.py
        test/patTuple_PF2PAT_cfg.py

--- python/tools/pfTools.py
+++ python/tools/pfTools.py
@@ -278,9 +278,9 @@ def switchToPFJets(process, input=cms.InputTag('pfNoTau'), algo='IC5', postfix =
         
     # changing the jet collection in PF2PAT:
     from PhysicsTools.PFCandProducer.Tools.jetTools import jetAlgo
-    inputCollection = getattr(process,"allPfJets"+postfix).src
-    setattr(process, "allPfJets"+postfix, jetAlgo( algo ) )
-    getattr(process,"allPfJets"+postfix).src = inputCollection
+    inputCollection = getattr(process,"pfJets"+postfix).src
+    setattr(process, "pfJets"+postfix, jetAlgo( algo ) )
+    getattr(process,"pfJets"+postfix).src = inputCollection
     switchJetCollection(process,
                         input,
                         jetIdLabel = algo,
@@ -296,6 +296,8 @@ def switchToPFJets(process, input=cms.InputTag('pfNoTau'), algo='IC5', postfix =
     
     applyPostfix(process, "patJets", postfix).embedCaloTowers   = False
     applyPostfix(process, "patJets", postfix).embedPFCandidates   = True
+    
+    
 
 #-- Remove MC dependence ------------------------------------------------------
 def removeMCMatchingPF2PAT( process, postfix="" ):
@@ -364,7 +366,7 @@ def usePF2PAT(process, runPF2PAT=True, jetAlgo='IC5', runOnMC=True, postfix = ""
     switchToPFMET(process, cms.InputTag('pfMET'+postfix), postfix=postfix)
     
     # Unmasked PFCandidates
-    addPFCandidates(process,cms.InputTag('pfNoJet'+postfix),patLabel='PFParticles',cut="",postfix=postfix)
+    addPFCandidates(process,cms.InputTag('pfNoJet'+postfix),patLabel='PFParticles'+postfix,cut="",postfix=postfix)
 
     if runOnMC:
         process.load("PhysicsTools.PFCandProducer.genForPF2PAT_cff")
