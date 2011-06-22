3bce3e745342baa974c8727d119c6673611bd1f4
cmsbuild <>
2011-06-22 13:12:33 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-V08-07-13'.
    Sprout from master 2011-06-07 18:44:39 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Fixing bug in 'addPFCandidates' with respect to the order of sequence definitions.'
    Cherrypick from master 2011-06-22 13:12:32 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Remove assertion again, since some paths indeed do not have active filters.':
        plugins/PATTriggerProducer.cc

--- plugins/PATTriggerProducer.cc
+++ plugins/PATTriggerProducer.cc
@@ -1,5 +1,5 @@
 //
-// $Id: PATTriggerProducer.cc,v 1.26 2011/03/16 13:03:53 vadler Exp $
+// $Id: PATTriggerProducer.cc,v 1.28 2011/06/21 23:27:55 vadler Exp $
 //
 
 
@@ -352,14 +352,16 @@ void PATTriggerProducer::produce( Event& iEvent, const EventSetup& iSetup )
       const std::string namePath( hltConfig_.triggerName( iP ) );
       const unsigned indexPath( hltConfig_.triggerIndex( namePath ) );
       const unsigned sizeModulesPath( hltConfig_.size( namePath ) );
-      const unsigned indexLastFilterPath( handleTriggerResults->index( indexPath ) );
-      unsigned indexLastFilterPathModules( indexLastFilterPath + 1 );
+      unsigned indexLastFilterPathModules( handleTriggerResults->index( indexPath ) + 1 );
       unsigned indexLastFilterFilters( sizeFilters );
       while ( indexLastFilterPathModules > 0 ) {
         --indexLastFilterPathModules;
-        const std::string labelLastFilterModules( hltConfig_.moduleLabel( indexPath, indexLastFilterPathModules ) );
-        indexLastFilterFilters = handleTriggerEvent->filterIndex( InputTag( labelLastFilterModules, "", nameProcess_ ) );
-        if ( indexLastFilterFilters < sizeFilters ) break;
+        const std::string labelLastFilterPathModules( hltConfig_.moduleLabel( indexPath, indexLastFilterPathModules ) );
+        indexLastFilterFilters = handleTriggerEvent->filterIndex( InputTag( labelLastFilterPathModules, "", nameProcess_ ) );
+        if ( indexLastFilterFilters < sizeFilters ) {
+          if ( hltConfig_.moduleType( labelLastFilterPathModules ) == "HLTBool" ) continue;
+          break;
+        }
       }
       for ( size_t iM = 0; iM < sizeModulesPath; ++iM ) {
         const std::string nameFilter( hltConfig_.moduleLabel( indexPath, iM ) );
@@ -373,9 +375,9 @@ void PATTriggerProducer::produce( Event& iEvent, const EventSetup& iSetup )
         }
       }
       if ( ! onlyStandAlone_ ) {
-        TriggerPath triggerPath( namePath, indexPath, hltConfig_.prescaleValue( set, namePath ), handleTriggerResults->wasrun( indexPath ), handleTriggerResults->accept( indexPath ), handleTriggerResults->error( indexPath ), indexLastFilterPath, hltConfig_.saveTagsModules( namePath ).size() );
+        TriggerPath triggerPath( namePath, indexPath, hltConfig_.prescaleValue( set, namePath ), handleTriggerResults->wasrun( indexPath ), handleTriggerResults->accept( indexPath ), handleTriggerResults->error( indexPath ), indexLastFilterPathModules, hltConfig_.saveTagsModules( namePath ).size() );
         // add module names to path and states' map
-        assert( indexLastFilterPath < sizeModulesPath );
+        assert( indexLastFilterPathModules < sizeModulesPath );
         std::map< unsigned, std::string > indicesModules;
         for ( size_t iM = 0; iM < sizeModulesPath; ++iM ) {
           const std::string nameModule( hltConfig_.moduleLabel( indexPath, iM ) );
@@ -398,9 +400,9 @@ void PATTriggerProducer::produce( Event& iEvent, const EventSetup& iSetup )
         triggerPaths->push_back( triggerPath );
         // cache module states to be used for the filters
         for ( std::map< unsigned, std::string >::const_iterator iM = indicesModules.begin(); iM != indicesModules.end(); ++iM ) {
-          if ( iM->first < indexLastFilterPath ) {
+          if ( iM->first < indexLastFilterPathModules ) {
             moduleStates[ iM->second ] = 1;
-          } else if ( iM->first == indexLastFilterPath ) {
+          } else if ( iM->first == indexLastFilterPathModules ) {
             moduleStates[ iM->second ] = handleTriggerResults->accept( indexPath );
           } else if ( moduleStates.find( iM->second ) == moduleStates.end() ) {
             moduleStates[ iM->second ] = -1;
@@ -426,8 +428,8 @@ void PATTriggerProducer::produce( Event& iEvent, const EventSetup& iSetup )
       const trigger::Vids & types = handleTriggerEvent->filterIds( iF );
       assert( types.size() == keys.size() );
       for ( size_t iK = 0; iK < keys.size(); ++iK ) {
-        filterLabels.insert( std::pair< trigger::size_type, std::string >( keys[ iK ], nameFilter ) ); // only for objects used in last active filter
-        objectTypes.insert( std::pair< trigger::size_type, int >( keys[ iK ], types[ iK ] ) );             // only for objects used in last active filter
+        filterLabels.insert( std::pair< trigger::size_type, std::string >( keys[ iK ], nameFilter ) );
+        objectTypes.insert( std::pair< trigger::size_type, int >( keys[ iK ], types[ iK ] ) );
       }
     }
 
