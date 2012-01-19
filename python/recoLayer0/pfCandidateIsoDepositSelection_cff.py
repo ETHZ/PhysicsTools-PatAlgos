bbddd6d508af90990100271cbbe39bc4ca126a7f
cmsbuild <>
2012-01-19 02:50:21 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- PatAlgos-V08-08-08'.
    Sprout from master 2012-01-19 02:50:20 UTC Tae Jeong Kim <sha1-32b43f0d0700c58cd86e2e757255aa5f208dff55@cern.ch> 'added charged particle (charged hadrons, e, and mu) isolation for muons and electrons'
    Cherrypick from master 2011-10-08 19:44:11 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Use correct input tags for RECO/AOD input, just as in CommonTools/ParticleFlow/python/PF2PAT_cff.py.':
        python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
        python/tools/coreTools.py
        python/tools/tauTools.py

--- python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
+++ python/recoLayer0/pfCandidateIsoDepositSelection_cff.py
@@ -1,9 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 from CommonTools.ParticleFlow.ParticleSelectors.pfSortByType_cff import *
-from CommonTools.ParticleFlow.pfNoPileUpIso_cff  import *
+from CommonTools.ParticleFlow.pfNoPileUp_cff import *
+
+pfPileUp.PFCandidates       = 'particleFlow'
+pfNoPileUp.bottomCollection = 'particleFlow'
 
 patPFCandidateIsoDepositSelection = cms.Sequence(
-       pfNoPileUpIsoSequence +
-       pfSortByTypeSequence
-       )
+    pfNoPileUpSequence *
+    ( pfAllNeutralHadrons +
+      pfAllChargedHadrons +
+      pfAllPhotons )
+    )
+
+
+
+
