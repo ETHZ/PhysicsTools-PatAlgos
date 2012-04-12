f6e94d495d5052ec0e724b371e1def41d8f87daf
Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch>
2012-04-12 08:38:13 +0000

    Switch off DAS usage.

--- python/patTemplate_cfg.py
+++ python/patTemplate_cfg.py
@@ -20,8 +20,9 @@ process.source = cms.Source("PoolSource",
 #                         , numberOfFiles = 1
 #                         )
 #     )
-       '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0082/38FB16A8-542B-E111-A9E6-003048FFCC1E.root',
-       '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0074/E871A010-DD2A-E111-83F5-001A928116CE.root'
+        '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0082/38FB16A8-542B-E111-A9E6-003048FFCC1E.root',
+        '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0074/E871A010-DD2A-E111-83F5-001A928116CE.root'
+    )
 )
 ## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
